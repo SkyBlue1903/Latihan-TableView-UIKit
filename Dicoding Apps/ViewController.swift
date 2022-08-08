62cdf91d953c1048666f258a997207bd27998e67
Erlangga Anugrah Arifin <angga1433@gmail.com>
2022-08-08 19:05:34 +0700

    Initial Commit

---
+++ Dicoding Apps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicoding Apps
+//
+//  Created by Erlangga Anugrah Arifin on 08/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
